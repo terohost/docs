892e1fe233f86fdbeb3c081c131ac6a0e7b88a24
Vaibhav Dhiman <vaibhavsys@protonmail.com>
2023-10-04 21:40:57 +0530

    chore: revert tina config

--- tina/config.js
+++ tina/config.js
@@ -20,83 +20,202 @@ export default defineConfig({
   },
   schema: {
     collections: [
-    {
-      name: "doc",
-      label: "Docs",
-      path: "content/docs",
-      frontmatterFormat: "toml",
-      frontmatterDelimiters: "+++",
-      fields: [
       {
-        type: "string",
-        name: "title",
-        label: "Title",
-        isTitle: true,
-        required: true,
-      },
-      {
-        type: "string",
-        name: "description",
-        label: "Description",
-        required: false,
-      },
-      {
-        type: "image",
-        name: "image",
-        label: "Image",
-        required: false,
-      },
-      {
-        type: "number",
-        name: "weight",
-        label: "Weight",
-        required: false,
-      },
-      {
-        type: "rich-text",
-        name: "body",
-        label: "Body",
-        isBody: true,
-      },
-      ],
-    },
-    {
-      name: "homepage",
-      label: "Homepage",
-      path: "content",
-      frontmatterFormat: "toml",
-      frontmatterDelimiters: "+++",
-      match: {
-        include: "*index*"
-      },
-      fields: [
-      {
-        type: "string",
-        name: "title",
-        label: "Title",
-        isTitle: true,
-        required: true,
+        name: "doc",
+        label: "Docs",
+        path: "content/docs",
+        frontmatterFormat: "toml",
+        frontmatterDelimiters: "+++",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: false,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+            required: false,
+          },
+          {
+            type: "number",
+            name: "weight",
+            label: "Weight",
+            required: false,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
       },
       {
-        type: "image",
-        name: "image",
-        label: "Image",
-        required: false,
+        name: "homepage",
+        label: "Homepage",
+        path: "content",
+        frontmatterFormat: "toml",
+        frontmatterDelimiters: "+++",
+        match: {
+          include: "*index*"
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+            required: false,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
       },
       {
-        type: "rich-text",
-        name: "body",
-        label: "Body",
-        isBody: true,
+        // date
+        // tags
+        // categories
+        name: "post",
+        label: "Posts",
+        path: "content/blog",
+        frontmatterFormat: "toml",
+        frontmatterDelimiters: "+++",
+        match: {
+          exclude: "*index.md"
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            ui: {
+              dateFormat: "YYYY-MM-DD"
+            },
+            required: false,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: false,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+            required: false,
+          },
+          {
+            name: "categories",
+            label: "Categories",
+            type: "string",
+            list: true,
+            searchable: false,
+            ui: {
+              validate: (value, data)=>{
+                const num_of_categories = value?.length;
+                if(num_of_categories > 7 ){
+                  return `Cannot exceed 7 ${value}`
+                }
+              }
+            }
+          },
+          {
+            name: "tag",
+            label: "tags",
+            type: "string",
+            list: true,
+            searchable: false,
+            ui: {
+              validate: (value, data)=>{
+                const num_of_categories = value?.length;
+                if(num_of_categories > 7 ){
+                  return `Cannot exceed 7 ${value}`
+                }
+              }
+            }
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
       },
-      ],
-    },
+      {
+        name: "tutorial",
+        label: "Tutorials",
+        path: "content/tutorials",
+        frontmatterFormat: "toml",
+        frontmatterDelimiters: "+++",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: false,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+            required: false,
+          },
+          {
+            type: "number",
+            name: "weight",
+            label: "Weight",
+            required: false,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      }
     ]
   },
   search: {
     tina: {
       indexerToken: process.env.TINA_SEARCH_TOKEN,
-      stopwordLanguages: ["eng"]
+      stopwordLanguages: ["deu", "eng", "fra", "ita", "spa", "nld"]
     },
     indexBatchSize: 100,
     maxSearchIndexFieldLength: 100
